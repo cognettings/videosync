c8e82a9b11d124bb44436e319291d47463e63ffe
John Cognetti <cognettings@gmail.com>
2015-12-09 12:39:18 -0500

    Use Bootstrap to create index page.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
+app.use(express.static('bower_components'));
+app.use(express.static('client'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.resolve(__dirname + '/../client/index.html'));
 });
 
 var server = app.listen(3000, function () {
